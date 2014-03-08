126cd9b77848492a240ddaaf5c30ba32e495efd0
tema <hellokatze@gmail.com>
2014-03-08 05:47:25 +0400

    Залит проект VS2013 + Node.js Tools (express, jade, stylus, mongoose нужно через npm посавить в node_modules).

---
+++ ExpressApp2/ExpressApp2/routes/bdjson.js
@@ -0,0 +1,19 @@
+﻿var mongoose = require('mongoose');
+
+var kittySchema = mongoose.Schema({
+    name: String
+})
+
+exports.bdjson =  function(req, res){
+    mongoose.connect('mongodb://localhost/test');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function callback () {
+	    var Kitten = mongoose.model('Kitten', kittySchema)
+	    Kitten.find({ name: new RegExp(req.query.q, 'i') }, function (err, kittens) {
+	      if (err) return console.error(err);
+          res.jsonp(kittens);
+          mongoose.disconnect();
+	    })
+    });
+};
