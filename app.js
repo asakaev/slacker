c69f4fe59f5bbcf24fa9fc21cdcfd210acbcc8c5
tema <tema@vf8.ru>
2014-03-27 01:29:13 +0400

    test app to linux server

---
+++ app.js
@@ -0,0 +1,16 @@
+var winston = require('winston');
+//winston.add(winston.transports.File, { filename: 'somefile.log' });
+//winston.remove(winston.transports.Console);
+
+// winston.remove(winston.transports.Console);
+//winston.log('info', 'Hello distributed log files!');
+//winston.info('Hello again distributed logs');
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello mofo lol123!\n');
+  //winston.info(new Date() + ': Who is there?');
+  // console.log(new Date());
+}).listen(1337, '192.168.1.2');
+console.log('Server running at http://192.168.1.2:1337/');
