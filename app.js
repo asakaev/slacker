a6e899d111accba82fa01098eed676adccb0d76c
tema <tema@vf8.ru>
2014-03-27 05:11:23 +0400

    Logger.

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-var winston = require('winston');
-//winston.add(winston.transports.File, { filename: 'somefile.log' });
-//winston.remove(winston.transports.Console);
-
-// winston.remove(winston.transports.Console);
-//winston.log('info', 'Hello distributed log files!');
-//winston.info('Hello again distributed logs');
+var ip = '192.168.1.2';
+var ip2 = 'localhost';
 
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Check near zero Nodemon update.\n');
-  //winston.info(new Date() + ': Who is there?');
-  // console.log(new Date());
-}).listen(1337, '192.168.1.2');
+  	console.log(new Date() + ': Who is there?');
+}).listen(1337, ip);
 console.log('Server running at http://192.168.1.2:1337/');
