d5276baf3c9d1862f201c1a25935319e25b81947
tema <tema@vf8.ru>
2014-04-01 13:05:35 +0400

    Не падает от бекслеша. Но и не ищет его.

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 ﻿function replaceAllBackSlash(targetStr){
       var index=targetStr.indexOf("\\");
       while(index >= 0){
-          targetStr=targetStr.replace("\\","");
+          targetStr=targetStr.replace("\\",'');
           index=targetStr.indexOf("\\");
       }
       return targetStr;
@@ -20,21 +20,26 @@ exports.dbGetJSON =  function(req, res){
     }
     else
     {
-      mongoose.connect('mongodb://dev.vf8.ru:443/test');
-      var db = mongoose.connection;
-      db.on('error', console.error.bind(console, 'connection error:'));
-      db.once('open', function callback () {
-        var Kitten = mongoose.model('Kitten', kittySchema)
+      var clean = replaceAllBackSlash(req.query.q);
+      if (clean === '')
+      {
+        res.jsonp([]);
+      }
+      else
+      {
+        mongoose.connect('mongodb://dev.vf8.ru:443/test');
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', function callback () {
+          var Kitten = mongoose.model('Kitten', kittySchema)
 
-        //var req = replaceAllBackSlash(req.query.q);
-        console.log(req.query.q);
-
-        Kitten.find({ name: new RegExp(req.query.q, 'i') }, function (err, kittens) {
-        if (err) return console.error(err);
-          res.jsonp(kittens);
-          mongoose.disconnect();
-        })
-
-      });
+          Kitten.find({ name: new RegExp(clean, 'i') }, function (err, kittens) {
+          if (err) return console.error(err);
+            res.jsonp(kittens);
+            mongoose.disconnect();
+          })
+          
+        });
+      }
     }
 };
