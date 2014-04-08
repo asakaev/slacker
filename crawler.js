f351a80e2a351f024063a67633737c4d6edf2c36
tema <tema@vf8.ru>
2014-04-08 15:17:22 +0400

    Проверка на отсутствующий телефон в боте и убрана версия в джейсоне.

--- crawler.js
+++ crawler.js
@@ -9,13 +9,11 @@ var vacanciesSchema = mongoose.Schema({
     sputnikId: String,
     tel: String,
     date: String
-});
+}, { versionKey: false });
 var vacancy = mongoose.model('Vacancy', vacanciesSchema);
-
-var done = 0;
+//var done = 0;
 
 function getPager(callback) {
-    //top-level code
     request('http://www.sputnik-cher.ru/301/', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             $ = cheerio.load(body);
@@ -30,9 +28,7 @@ function getContent(pageNum) {
     request('http://www.sputnik-cher.ru/301/?p=' + pageNum, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             $ = cheerio.load(body);
-
             var date = $('.dateBut')["1"].children["1"].children["0"].data;
-
             var nodes = $('.itemOb');
             nodes.each(function (index) {
                 var obj = {};
@@ -40,33 +36,27 @@ function getContent(pageNum) {
                 var text = nodes[index].children["4"].data;
                 obj.text = text.substring(1, text.length - 7);
                 obj.sputnikId = nodes[index].children["1"].attribs.name;
-                obj.tel = nodes[index].children["5"].children["0"].data;
+
+                if(nodes[index].children["5"].children["0"] !== undefined) {
+                    obj.tel = nodes[index].children["5"].children["0"].data;
+                }
+
                 obj.date = date;
                 //console.log(obj.vacancy);
-
                 var testVac = new vacancy(obj);
                 testVac.save();
-                console.log(++done);
+                //console.log(++done);
             }); // end of DOM traversal
         }
     });
 }
 
 function pagesLoop(pages, callback) {
-    for (var i = 1; i <= 2; i++) {
+    for (var i = 1; i <= pages; i++) {
         console.log('Getting page: ' + i);
         getContent(i);
     }
     callback();
-}
-
-function businessLogic() {
-    //uses the above to get the work done
-    console.log('Connecting to DB.');
-    mongoose.connect('mongodb://ubuntu/work');
-    getPager(function (pagesCount) {
-        pagesLoop(pagesCount, Done);
-    });
 }
 
 function Done() {
@@ -74,4 +64,12 @@ function Done() {
     console.log('Done');
 }
 
-businessLogic();
+function Run() {
+    console.log('Connecting to DB.');
+    mongoose.connect('mongodb://dev.vf8.ru:443/work');
+    getPager(function (pagesCount) {
+        pagesLoop(pagesCount, Done);
+    });
+}
+
+Run();
