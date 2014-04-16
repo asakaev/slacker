9c5ef2ac41c1f01625b710fa798cc8e4cc97163f
tema <tema@vf8.ru>
2014-04-17 01:48:04 +0400

    Just code reformat.

--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,30 @@
-﻿var mongoose = require( 'mongoose' );
-var Vacancy = mongoose.model( 'Vacancy' );
+﻿var mongoose = require('mongoose');
+var Vacancy = mongoose.model('Vacancy');
 
-function replaceAllBackSlash(targetStr){
-      var index=targetStr.indexOf("\\");
-      while(index >= 0){
-          targetStr=targetStr.replace("\\",'');
-          index=targetStr.indexOf("\\");
-      }
-      return targetStr;
-  }
+function replaceAllBackSlash(targetStr) {
+    var index = targetStr.indexOf("\\");
+    while (index >= 0) {
+        targetStr = targetStr.replace("\\", '');
+        index = targetStr.indexOf("\\");
+    }
+    return targetStr;
+}
 
-
-exports.dbGetJSON =  function(req, res){
+exports.dbGetJSON = function (req, res) {
 
     if (typeof req.query.q === 'undefined') {
-      res.jsonp([]);
-    }
-    else
-    {
-      var clean = replaceAllBackSlash(req.query.q);
-      if (clean === '')
-      {
         res.jsonp([]);
-      }
-      else
-      {
-        Vacancy.find({ vacancy: new RegExp(clean, 'i') }, '-sputnikId -_id', function (err, vacancies) {
-        if (err) return console.error(err);
-          res.jsonp(vacancies);
-        }) // end vacancies find
-      }
+    }
+    else {
+        var clean = replaceAllBackSlash(req.query.q);
+        if (clean === '') {
+            res.jsonp([]);
+        }
+        else {
+            Vacancy.find({ vacancy: new RegExp(clean, 'i') }, '-sputnikId -_id', function (err, vacancies) {
+                if (err) return console.error(err);
+                res.jsonp(vacancies);
+            }); // end vacancies find
+        }
     }
 };
