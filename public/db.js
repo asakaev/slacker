5bf7fbf0fcf9732f109eff8971d72aad7545cf49
rtrdd <rtrdd@yandex.ru>
2014-04-22 19:45:01 +0400

    db.js сломался. И починился.

---
+++ public/db.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var sputnikSchema = mongoose.Schema({
+    vacancy: String,
+    text: String,
+    idSputnik: Number,
+    tel: String,
+    added: Date,
+    issue: Number
+}, { versionKey: false,
+    collection: 'sputnik'});
+
+mongoose.model('Vacancy', sputnikSchema);
+mongoose.connect('mongodb://localhost/work');
