3c7ab6813f05ebea62baecf20f1eaebc6755f405
rtrdd <rtrdd@yandex.ru>
2014-06-20 15:23:30 +0400

    Привет с защиты

--- public/js/searchlib.js
+++ public/js/searchlib.js
@@ -72,7 +72,7 @@ var Search = {
 				// $(".content").empty();
 				$("content").css({"display": "block"});
 				$.each(data, function(i,val) {
-					var listItem = "<li><div class = \"vacancy\"><a href = \"#\">"+val.vacancy+"</a></div><div class = \"description\">"+val.text+"</div></li>";
+					var listItem = "<li><div class = \"vacancy\">"+val.vacancy+"</div><div class = \"description\"> Условия работы: "+val.text+"<br> Телефон: " + val.tel + "</div></li>";
 					$(".content_ul").append(listItem);
 				})
 				
